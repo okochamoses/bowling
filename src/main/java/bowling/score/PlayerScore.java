6e3a548abe47b7b17880a17904fddb18d9b38016
Okocha Moses <okocha.chukwutem@gmail.com>
2022-11-17 19:01:42 +0100

    add logic to update frames sequentially

--- src/main/java/bowling/score/PlayerScore.java
+++ src/main/java/bowling/score/PlayerScore.java
@@ -2,17 +2,29 @@ package bowling.score;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 public class PlayerScore {
   private final Player player;
-  private final List<Frame> frames;
+  private final Frame[] frames;
+  private int currentFrame;
 
   public PlayerScore(String playerName) {
     this.player = new Player(playerName);
-    this.frames = new ArrayList<>();
+    this.frames = new Frame[10];
+    this.currentFrame = 0;
+  }
+
+  public void addPinFall(int pinFall) {
+    Frame frame = getCurrentFrame();
+    frame.addPinFall(pinFall);
+    if(frame.isFilled()) currentFrame++;
+  }
+
+  private Frame getCurrentFrame() {
+    if(frames[currentFrame] == null) {
+      frames[currentFrame] = new Frame();
+    }
+    return frames[currentFrame];
   }
 
 }
--- src/test/java/bowling/score/PlayerScoreTest.java
+++ src/test/java/bowling/score/PlayerScoreTest.java
@@ -3,7 +3,8 @@ package bowling.score;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class PlayerScoreTest {
   PlayerScore playerScore;
@@ -20,7 +21,30 @@ class PlayerScoreTest {
 
   @Test
   void shouldContainFramesContainer() {
-
     assertNotNull(playerScore.getFrames());
+  }
+  @Test
+  void shouldAddPinFallsSequentiallyssss() {
+    playerScore.addPinFall(1);
+    playerScore.addPinFall(5);
+
+    Frame frame1 = playerScore.getFrames()[0];
+
+    assertEquals(1, frame1.getFirst());
+    assertEquals(5, frame1.getSecond());
+  }
+
+  @Test
+  void shouldPopulateNextFrameWhenPreviousFrameIsFilled() {
+    playerScore.addPinFall(1);
+    playerScore.addPinFall(5);
+    playerScore.addPinFall(3);
+
+    Frame frame1 = playerScore.getFrames()[0];
+    Frame frame2 = playerScore.getFrames()[1];
+
+    assertEquals(1, frame1.getFirst());
+    assertEquals(5, frame1.getSecond());
+    assertEquals(3, frame2.getFirst());
   }
 }
